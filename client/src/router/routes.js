e3827d6377f217cb09d391c839164c1d604db294
João Lucas <joaolucascp16@gmail.com>
2023-08-26 09:00:38 -0300

    setting routes

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -1,19 +1,32 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
+  },
+  {
+    path: "/login",
+    component: () => import("layouts/LoginLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      {
+        path: "login",
+        name: "login",
+        component: () => import("pages/login.vue"),
+      },
+      {
+        path: "",
+        name: "loginDefault",
+        component: () => import("pages/login.vue"),
+      },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
