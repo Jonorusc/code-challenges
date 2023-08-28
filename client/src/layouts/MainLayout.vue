d5a02abfc5f42f26b6ea298c89fd68245c838ea8
João Lucas <joaolucascp16@gmail.com>
2023-08-27 23:53:10 -0300

    feat(layouts/MainLayout): checks whether the token has expired

--- client/src/layouts/MainLayout.vue
+++ client/src/layouts/MainLayout.vue
@@ -7,13 +7,41 @@
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
+import { defineComponent } from "vue";
+import jwt_decode from "jwt-decode";
 
 export default defineComponent({
   name: "MainLayout",
 
   setup() {
-    return {};
+    return {
+      // active this when you want to avoid the token check
+      // (off course, this is not a good practice and i'm doind this just for the sake of simplicity during my app development)
+      debug: true,
+    };
+  },
+  beforeMount() {
+    if (!this.debug) {
+      const token = localStorage.getItem("token");
+      // if the token is empty it means that there is nothing in localstorage, so we won't pass it on
+      // check whether the token has expired
+      if (
+        token === null ||
+        jwt_decode(token).exp <= Math.floor(new Date() / 1000)
+      ) {
+        // expired
+        this.$q.notify({
+          color: "negative",
+          position: "top",
+          message: "Sua sessão expirou!",
+          timeout: 1500,
+        });
+        localStorage.removeItem("token");
+        this.$router.push("/login");
+      } else {
+        // not expired
+      }
+    }
   },
 });
 </script>
