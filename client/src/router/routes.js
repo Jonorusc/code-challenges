05d0f1133c77a8c98c52d4c4a72690a07cc6e501
João Lucas <joaolucascp16@gmail.com>
2023-09-06 21:21:08 -0300

    feat: using AuthMiddleware

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -1,8 +1,24 @@
+import jwt_decode from "jwt-decode";
+const Authmiddleware = (to, from, next) => {
+  const token = localStorage.getItem("token");
+
+  if (
+    to.path === "/" &&
+    (!token || jwt_decode(token).exp <= Math.floor(new Date() / 1000))
+  ) {
+    localStorage.removeItem("token");
+    next("/login");
+  } else {
+    next();
+  }
+};
+
 const routes = [
   {
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
     children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
+    beforeEnter: Authmiddleware,
   },
   {
     path: "/login",
